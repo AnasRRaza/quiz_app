a3d823c55ea51e96ffcf200c7b07f1307dd386b9
Anas Raza <anus.raza987@gmail.com>
2021-10-21 15:13:13 +0500

    Add Styling and make Mobile responsive

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -3,37 +3,44 @@ import { questionPropTypes } from '../types';
 
 const Cards: FC<questionPropTypes> = (props) => {
 
-  const { questions, options, handleSubmit } = props;
+  const { questions, options, handleSubmit, questionNumber, totalQuestions } = props;
 
   const [userAns, setUserAns] = useState("");
 
   const handleSubmitedAns = (e: any) => {
     setUserAns(e.target.value)
   }
+  const btnText = questionNumber === 4 ? "Submit" : "Next";
 
   return (
-    <div>
-      <p>{questions}</p>
+    <div className="cards">
+      <div className="center">
+        <h3 className="question-heading center">Question: {questionNumber + 1}/{totalQuestions}</h3>
+      </div>
+      <p>Q{questionNumber + 1}: {questions}</p>
       <form onSubmit={(e: React.FormEvent<EventTarget>) => handleSubmit(e, userAns)}>
         {
           options.map((val: string, ind: number) => {
             return (
-              <label key={ind}>
-                <input
-                  type="radio"
-                  required={true}
-                  value={val}
-                  onChange={handleSubmitedAns}
-                  checked={userAns === val}
-                  name="options"
-                />
-                {val}
-              </label>
+              <div key={ind}>
+                <label >
+                  <input
+                    type="radio"
+                    required={true}
+                    value={val}
+                    onChange={handleSubmitedAns}
+                    checked={userAns === val}
+                    name="options"
+                  />
+                  {val}
+                </label>
+                <br />
+              </div>
             )
           })
         }
         <br />
-        <input type="submit" value="Submit" />
+        <input type="submit" value={btnText} className="btn" />
       </form>
     </div>
   )
--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -7,10 +7,21 @@ const ResultCard: FC<ResultPropTypes> = (props) => {
   const { score, totalQuestions, tryAgain } = props;
 
   return (
-    <div>
-      <h1>Result</h1>
-      <p>Your Score is {score} out of {totalQuestions}</p>
-      <input type="button" value="Restart" onClick={tryAgain} />
+    <div className="result-card">
+      <h1 className="result">Result</h1>
+      <div className="result-items">
+        <h2>Total Questions:</h2>
+        <h2>{totalQuestions}</h2>
+      </div>
+      <div className="result-items">
+        <h2>Correct Answers:</h2>
+        <h2>{score}</h2>
+      </div>
+      <div className="result-items">
+        <h2>Percentage:</h2>
+        <h2>{(score / 5) * 100}</h2>
+      </div>
+      <input type="button" value="Restart" onClick={tryAgain} className="restart-btn" />
     </div>
   )
 }
