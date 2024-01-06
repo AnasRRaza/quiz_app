3ee292ed21d163e1b24fccb8788f61818cec42b9
Anas Raza <anus.raza987@gmail.com>
2024-01-07 00:16:44 +0500

    fix: fixed HTML entities issue

--- src/common/utils.tsx
+++ src/common/utils.tsx
@@ -1,5 +1,9 @@
-
-
 export const shuffleArray = (array: any[]) => {
   return [...array].sort(() => Math.random() - 0.5);
-}
+};
+
+export const decodeHTMLEntities = (str: string) => {
+  const textArea = document.createElement("textarea");
+  textArea.innerHTML = str;
+  return textArea.value;
+};
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,49 +1,56 @@
-import React, { FC, useState } from 'react'
-import { questionPropTypes } from '../types';
+import React, { FC, useState } from "react";
+import { questionPropTypes } from "../types";
+import { decodeHTMLEntities } from "../common/utils";
 
 const Cards: FC<questionPropTypes> = (props) => {
-
-  const { questions, options, handleSubmit, questionNumber, totalQuestions } = props;
+  const { questions, options, handleSubmit, questionNumber, totalQuestions } =
+    props;
 
   const [userAns, setUserAns] = useState("");
 
   const handleSubmitedAns = (e: any) => {
-    setUserAns(e.target.value)
-  }
+    setUserAns(e.target.value);
+  };
   const btnText = questionNumber === 4 ? "Submit" : "Next";
+
+  const decodedQuestion = decodeHTMLEntities(questions);
 
   return (
     <div className="cards">
       <div className="center">
-        <h3 className="question-heading center">Question: {questionNumber + 1}/{totalQuestions}</h3>
+        <h3 className="question-heading center">
+          Question: {questionNumber + 1}/{totalQuestions}
+        </h3>
       </div>
-      <p>Q{questionNumber + 1}: {questions}</p>
-      <form onSubmit={(e: React.FormEvent<EventTarget>) => handleSubmit(e, userAns)}>
-        {
-          options.map((val: string, ind: number) => {
-            return (
-              <div key={ind}>
-                <label >
-                  <input
-                    type="radio"
-                    required={true}
-                    value={val}
-                    onChange={handleSubmitedAns}
-                    checked={userAns === val}
-                    name="options"
-                  />
-                  {val}
-                </label>
-                <br />
-              </div>
-            )
-          })
-        }
+      <p>
+        Q{questionNumber + 1}: {decodedQuestion}
+      </p>
+      <form
+        onSubmit={(e: React.FormEvent<EventTarget>) => handleSubmit(e, userAns)}
+      >
+        {options.map((val: string, ind: number) => {
+          return (
+            <div key={ind}>
+              <label>
+                <input
+                  type="radio"
+                  required={true}
+                  value={val}
+                  onChange={handleSubmitedAns}
+                  checked={userAns === val}
+                  name="options"
+                />
+                {val}
+              </label>
+              <br />
+            </div>
+          );
+        })}
         <br />
         <input type="submit" value={btnText} className="btn" />
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Cards;
